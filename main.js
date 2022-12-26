f8132de7f92876e211658d67ef4a5da283087fc0
thaislem <phamthai240699@gmail.com>
2022-12-26 18:13:42 +0700

    update

--- main.js
+++ main.js
@@ -8,23 +8,38 @@ function showDate() {
 }
 function showMounth() {
     const d = new Date();
-    document.getElementById("mounth").innerHTML = d.getMonth();
+    document.getElementById("mounth").innerHTML = d.getMonth() + 1;
 }
 function showYear() {
     const d = new Date();
     document.getElementById("year").innerHTML = d.getFullYear();
 }
-function daysLeft() {
-    const d = new Date();
-    const nam = 2023- d.getFullYear();
-    const thang= 1- d.getMonth();
-    const ngay =22- d.getDate();
-    const daysLeft = nam*12*60 + thang*12 + ngay;
-    document.getElementById("days__left").innerHTML = 29;
+
+var daysLeft = document.getElementById("days__left");
+var hoursleft = document.getElementById("hours__left");
+var minuteleft = document.getElementById("minute__left");
+var secondleft = document.getElementById("second__left");
+var countDownDate =new Date("January 21 2023 23:59:59").getTime();
+
+function dateDiffFs() {
+    let dateNow = new Date().getTime();
+    let dateDiff = countDownDate - dateNow;
+    let day = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hour = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) ;
+    let minute = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let second = Math.floor((dateDiff % (1000 * 60)) / (1000));
+    daysLeft.innerHTML = day;
+    hoursleft.innerHTML = hour;
+    minuteleft.innerHTML = minute;
+    secondleft.innerHTML = second;
+    
+    if(dateDiff == 0){
+        clearInterval(counter);
+    }
 }
 
 
-daysLeft();
+setInterval(dateDiffFs, 1000)
 setInterval(showTime, 1000);
 showDate();
 showMounth();
